faddfe7aa742745db72ee42347937f65397fbd57
Viviana <leidy.1701913815@ucaldas.edu.co>
2023-04-10 21:54:24 -0500

    Lista de productos locales

--- lib/Screens/product_list.dart
+++ lib/Screens/product_list.dart
@@ -54,17 +54,41 @@ class _ProductListState extends State<ProductList> {
         unit: 'Kg',
         price: 90000,
         image: 'assets/images/termosPareja.png'),
-//  Item(name: 'Peach', unit: 'Pc', price: 8, image: 'assets/images/peach.png'),
-//  Item(
-//      name: 'Strawberry',
-//      unit: 'Box',
-//      price: 12,
-//      image: 'assets/images/strawberry.png'),
-//  Item(
-//      name: 'Fruit Basket',
-//      unit: 'Kg',
-//      price: 55,
-//      image: 'assets/images/fruitBasket.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 180000,
+        image: 'assets/images/chaquetaMi.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 185000,
+        image: 'assets/images/chaquetaCal.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 120000,
+        image: 'assets/images/chaquetaFlores.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 190000,
+        image: 'assets/images/chaquetaGat.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 20000,
+        image: 'assets/images/pocilloHotel.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 19000,
+        image: 'assets/images/pocilloMaestro.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 15000,
+        image: 'assets/images/pocilloLove.png'),
   ];
 
   @override
