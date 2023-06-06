fe93747ca87bf7784d1a1bd427213e481ba8255a
Brayan Stiven Cifuentes <1704stiven@gmail.com>
2023-06-05 19:12:28 -0500

    se realizaron algunas refactorizaciones

---
+++ lib/Screens/card_item_list.dart
@@ -0,0 +1,94 @@
+
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:estampados_app/Model/cart_model.dart';
+import 'package:estampados_app/Model/item_model.dart';
+import 'package:estampados_app/dataBase/bd_helper.dart';
+import 'package:estampados_app/Provider/cart_provider.dart';
+
+
+
+
+
+class DataRepository {
+ late BuildContext? context;
+
+  DataRepository( {context});
+  List<Item> products = [
+    Item(
+        name: 'Camiseta Negra',
+        unit: 'Kg',
+        price: 50000,
+        image: 'assets/images/camisetaAkat.png'),
+    Item(
+        name: 'Camiseta Blanca',
+        unit: 'Kg',
+        price: 60000,
+        image: 'assets/images/camisetaGato.png'),
+    Item(
+        name: 'Camiseta Blanca',
+        unit: 'Doz',
+        price: 40000,
+        image: 'assets/images/camisetaGatoCorazones.png'),
+    Item(
+        name: 'Camiseta Blanca',
+        unit: 'Kg',
+        price: 100000,
+        image: 'assets/images/camisetaNaruto.png'),
+    Item(
+        name: 'Camiseta Negra',
+        unit: 'Kg',
+        price: 75000,
+        image: 'assets/images/camisetaNegraEstrella.png'),
+    Item(
+        name: 'Termos',
+        unit: 'Kg',
+        price: 70000,
+        image: 'assets/images/termos.png'),
+    Item(
+        name: 'Termos Pareja',
+        unit: 'Kg',
+        price: 90000,
+        image: 'assets/images/termosPareja.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 180000,
+        image: 'assets/images/chaquetaMi.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 185000,
+        image: 'assets/images/chaquetaCal.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 120000,
+        image: 'assets/images/chaquetaFlores.png'),
+    Item(
+        name: 'Chaqueta',
+        unit: 'Kg',
+        price: 190000,
+        image: 'assets/images/chaquetaGat.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 20000,
+        image: 'assets/images/pocilloHotel.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 19000,
+        image: 'assets/images/pocilloMaestro.png'),
+    Item(
+        name: 'Pocillo',
+        unit: 'Kg',
+        price: 15000,
+        image: 'assets/images/pocilloLove.png'),
+  ];
+
+ get getProducts {
+  return products;
+ }
+
+}
--- lib/Screens/product_list.dart
+++ lib/Screens/product_list.dart
@@ -1,3 +1,4 @@
+import 'package:estampados_app/Screens/card_item_list.dart';
 import 'package:estampados_app/Screens/cart_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,80 +19,8 @@ class ProductList extends StatefulWidget {
 
 class _ProductListState extends State<ProductList> {
   DBHelper dbHerper = DBHelper();
-
-  List<Item> products = [
-    Item(
-        name: 'Camiseta Negra',
-        unit: 'Kg',
-        price: 50000,
-        image: 'assets/images/camisetaAkat.png'),
-    Item(
-        name: 'Camiseta Blanca',
-        unit: 'Kg',
-        price: 60000,
-        image: 'assets/images/camisetaGato.png'),
-    Item(
-        name: 'Camiseta Blanca',
-        unit: 'Doz',
-        price: 40000,
-        image: 'assets/images/camisetaGatoCorazones.png'),
-    Item(
-        name: 'Camiseta Blanca',
-        unit: 'Kg',
-        price: 100000,
-        image: 'assets/images/camisetaNaruto.png'),
-    Item(
-        name: 'Camiseta Negra',
-        unit: 'Kg',
-        price: 75000,
-        image: 'assets/images/camisetaNegraEstrella.png'),
-    Item(
-        name: 'Termos',
-        unit: 'Kg',
-        price: 70000,
-        image: 'assets/images/termos.png'),
-    Item(
-        name: 'Termos Pareja',
-        unit: 'Kg',
-        price: 90000,
-        image: 'assets/images/termosPareja.png'),
-    Item(
-        name: 'Chaqueta',
-        unit: 'Kg',
-        price: 180000,
-        image: 'assets/images/chaquetaMi.png'),
-    Item(
-        name: 'Chaqueta',
-        unit: 'Kg',
-        price: 185000,
-        image: 'assets/images/chaquetaCal.png'),
-    Item(
-        name: 'Chaqueta',
-        unit: 'Kg',
-        price: 120000,
-        image: 'assets/images/chaquetaFlores.png'),
-    Item(
-        name: 'Chaqueta',
-        unit: 'Kg',
-        price: 190000,
-        image: 'assets/images/chaquetaGat.png'),
-    Item(
-        name: 'Pocillo',
-        unit: 'Kg',
-        price: 20000,
-        image: 'assets/images/pocilloHotel.png'),
-    Item(
-        name: 'Pocillo',
-        unit: 'Kg',
-        price: 19000,
-        image: 'assets/images/pocilloMaestro.png'),
-    Item(
-        name: 'Pocillo',
-        unit: 'Kg',
-        price: 15000,
-        image: 'assets/images/pocilloLove.png'),
-  ];
-
+  DataRepository repository = DataRepository();
+  late List<Item> products = repository.getProducts;
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<CartProvider>(context);
@@ -229,7 +158,7 @@ class _ProductListState extends State<ProductList> {
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blueGrey.shade800),
                     onPressed: () {
-                      savedData(index);
+                    savedData(index);
                     },
                     child: const Text('Añadir al Carrito'),
                   )
